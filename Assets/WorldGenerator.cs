56a1a4f986f00cc85d3d2b49de54d43721c76279
Thiccz732 <achmadmarcello732@gmail.com>
2026-04-29 08:27:09 +0700

    Menambahkan RNG wall

--- Assets/WorldGenerator.cs
+++ Assets/WorldGenerator.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject wallPrefab; // Masukkan prefab tembok/block di sini
+    public int numberOfWalls = 10; // Jumlah tembok yang ingin di-spawn
+    public Vector2 spawnArea = new Vector2(10, 10); // Rentang area (X, Y)
+
     void Start()
     {
-        
+        GenerateWalls();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateWalls()
     {
-        
+        for (int i = 0; i < numberOfWalls; i++)
+        {
+            // Menghitung posisi acak di dalam jangkauan spawnArea
+            float randomX = Random.Range(-spawnArea.x / 2, spawnArea.x / 2);
+            float randomY = Random.Range(-spawnArea.y / 2, spawnArea.y / 2);
+            Vector3 spawnPos = new Vector3(randomX, randomY, 0);
+
+            // Munculkan tembok
+            Instantiate(wallPrefab, spawnPos, Quaternion.identity, transform);
+        }
+    }
+
+    // Menggambar kotak area di Editor untuk membantu visualisasi
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(spawnArea.x, spawnArea.y, 0));
     }
 }
